699a1fa37b457d22b3e727c8db16330adeae4ca5
gsarvagya45 <gsarvagya45@gmail.com>
2024-12-13 23:44:39 +0530

    api making file

---
+++ data.js
@@ -0,0 +1,6 @@
+const data=[
+    {name: 'sarvagya',email:'gsarvagya45@gmail.com'},
+    {name: 'kushagra',email:'gkushagra45@gmail.com'},
+    {name: 'mridul',email:'gmridul45@gmail.com'}
+];
+module.exports ={data};
