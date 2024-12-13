e1d263c5695a2e2ffb513e43f9619af3c494c793
gsarvagya45 <gsarvagya45@gmail.com>
2024-12-13 23:02:39 +0530

    comment in core module file

--- coremodule.js
+++ coremodule.js
@@ -2,3 +2,4 @@ const fs=require('fs');
 fs.writeFileSync("hello.txt","code step by step");
 console.log("->",__dirname);
 console.log("->",__filename);
+//core modules are modules that are impoted when we need to use them
